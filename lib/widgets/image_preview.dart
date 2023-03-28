04ab4d47d8f395d7927ea2606b3d9c0dd51dddf0
adam.kwiatkowski <adam.kwiatkowski256@gmail.com>
2023-03-28 11:19:59 +0200

    Add image original size option

--- lib/widgets/image_preview.dart
+++ lib/widgets/image_preview.dart
@@ -2,7 +2,7 @@ import 'dart:ui' as ui;
 
 import 'package:flutter/material.dart';
 
-class ImagePreview extends StatelessWidget {
+class ImagePreview extends StatefulWidget {
   const ImagePreview({
     super.key,
     required ui.Image? image,
@@ -11,12 +11,21 @@ class ImagePreview extends StatelessWidget {
   final ui.Image? _image;
 
   @override
+  State<ImagePreview> createState() => _ImagePreviewState();
+}
+
+class _ImagePreviewState extends State<ImagePreview> {
+  bool _originalSize = false;
+  final TransformationController _transformationController =
+      TransformationController();
+
+  @override
   Widget build(BuildContext context) {
     return AspectRatio(
       aspectRatio: 1,
       child: Card(
         clipBehavior: Clip.antiAlias,
-        child: _image == null
+        child: widget._image == null
             ? Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: const [
@@ -27,11 +36,34 @@ class ImagePreview extends StatelessWidget {
                   Text('Filter results will appear here'),
                 ],
               )
-            : InteractiveViewer(
-                child: RawImage(
-                  image: _image!,
+            : Stack(children: [
+                Positioned.fill(
+                  child: InteractiveViewer(
+                    constrained: !_originalSize,
+                    minScale: _originalSize ? 1 : 0.5,
+                    maxScale: _originalSize ? 1 : 2.5,
+                    transformationController: _transformationController,
+                    child: RawImage(
+                      image: widget._image!,
+                    ),
+                  ),
                 ),
-              ),
+                Positioned(
+                  top: 5,
+                  right: 5,
+                  child: IconButton(
+                    icon: Icon(
+                        _originalSize ? Icons.zoom_out_map : Icons.zoom_in),
+                    onPressed: () {
+                      _transformationController.value = Matrix4.identity();
+                      setState(() {
+                        _originalSize = !_originalSize;
+                      });
+                    },
+                    tooltip: _originalSize ? 'Fit to screen' : 'Original size',
+                  ),
+                ),
+              ]),
       ),
     );
   }
